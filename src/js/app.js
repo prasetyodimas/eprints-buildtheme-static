61d10038847222fd8e47f5050d5282cf6f47e950
Dimas Prasetyo <prasetyo.dimas@pricebook.co.id>
2020-10-25 11:02:56 +0700

    development style and login area

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 console.info(' erpints static bundle main js !');
-// core eprints
-// require ('./core/auto-3.4.1.js');
-require ('./utils/skel.js');
-require ('./utils/utils.js');
-require ('./utils/main.js');
+
+require ('./core/jquery.min.js');
+// require ('./utils/skel.js');
+// require ('./utils/utils.js');
+// require ('./utils/main.js');
+  
